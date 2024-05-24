f333d145d4b7469052598ee1a888980c4bd09f9d
volkinuna <plleiades@gmail.com>
2024-05-24 14:13:00 +0900

    로그인 기능 구현

--- src/main/java/com/seohauniv/controller/MemberController.java
+++ src/main/java/com/seohauniv/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.seohauniv.controller;
 
+import com.seohauniv.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class MemberController {
     // 로그인 실패시
     @GetMapping(value = "/members/login/error")
     public String loginError(Model model) {
-        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+        model.addAttribute("loginErrorMsg", "학번/교번 또는 비밀번호를 확인해주세요.");
         return "member/memberLoginForm"; // 로그인 페이지로 그대로 이동
     }
 
