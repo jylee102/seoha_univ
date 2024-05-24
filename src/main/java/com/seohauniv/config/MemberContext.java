094a72f65a3efc0e88564b3a0e8d62abbec25d2d
jylee102 <lilydodo11@gmail.com>
2024-05-24 16:58:08 +0900

    MemberContext 수정 및 사이드바 개선

--- src/main/java/com/seohauniv/config/MemberContext.java
+++ src/main/java/com/seohauniv/config/MemberContext.java
@@ -1,6 +1,5 @@
 package com.seohauniv.config;
 
-import com.seohauniv.constant.Role;
 import com.seohauniv.entity.Member;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,12 +13,12 @@ public class MemberContext extends User {
     //Authentication 객체에 저장하고 싶은 값을 필드로 지정
     private final String email;
     private final String name;
-    private final Role role;
+    private final String role;
 
     public MemberContext(Member member, List<GrantedAuthority> authorities) {
         super(member.getId(), member.getPassword(), authorities); //Member 생성자 실행
         this.email = member.getEmail();
         this.name = member.getName();
-        this.role = member.getRole();
+        this.role = member.getRole().toString();
     }
 }
