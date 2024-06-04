b26819196ed1707ecf5c70b39418349f94fd8073
parkjeongmin1 <gars150@naver.com>
2024-06-04 15:15:19 +0900

    공지사항 삭제 및 인덱스 홈페이지 공지사항 구현

--- src/main/java/com/seohauniv/controller/NoticeController.java
+++ src/main/java/com/seohauniv/controller/NoticeController.java
@@ -9,14 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -111,4 +111,13 @@ public class NoticeController {
         return "redirect:/notice/list";
     }
 
+    //공지삭제
+    @DeleteMapping("/notice/{noticeId}/delete")
+    public @ResponseBody ResponseEntity deleteNotice(@PathVariable("noticeId") Long noticeId
+            ) {
+
+        noticeService.deleteNotice(noticeId);
+
+        return new ResponseEntity<Long>(noticeId, HttpStatus.OK);
+    }
 }
--- src/main/java/com/seohauniv/service/NoticeService.java
+++ src/main/java/com/seohauniv/service/NoticeService.java
@@ -62,4 +62,16 @@ public class NoticeService {
         NoticeFormDto noticeFormDto = NoticeFormDto.of(notice);
         return noticeFormDto;
     }
+
+    //공지 삭제
+    public void deleteNotice(Long noticeId) {
+        //★delete하기 전에 select 를 한번 해준다
+        //->영속성 컨텍스트에 엔티티를 저장한 후 변경 감지를 하도록 하기 위해
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        //delete
+        //CasCade 설정을 통해 order의 자식 엔티티에 해당하는 orderItem 도 같이 삭제된다.
+        noticeRepository.delete(notice);
+    }
 }
