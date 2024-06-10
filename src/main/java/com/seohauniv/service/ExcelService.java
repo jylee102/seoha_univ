aca22e48d88f8bab4f2dd1316d4dc00fe03130ca
jylee102 <lilydodo11@gmail.com>
2024-06-10 16:25:33 +0900

    강의계획서 검색,프로그래스바, datePicker

--- src/main/java/com/seohauniv/service/ExcelService.java
+++ src/main/java/com/seohauniv/service/ExcelService.java
@@ -1,6 +1,7 @@
 package com.seohauniv.service;
 
 import com.seohauniv.dto.MemberFormDto;
+import com.seohauniv.dto.ProgressUpdate;
 import com.seohauniv.entity.Dept;
 import com.seohauniv.entity.Professor;
 import com.seohauniv.entity.Staff;
@@ -10,6 +11,9 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
@@ -26,28 +30,33 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ExcelService {
     private final DeptService deptService;
+    private final MemberService memberService;
     private final Validator memberFormDtoValidator;
 
-    public Map<String, Object> readExcelFile(MultipartFile file) throws IOException {
+    public String readExcelFile(MultipartFile file, SimpMessagingTemplate messagingTemplate) throws IOException {
         List<Staff> staffs = new ArrayList<>();
         List<Student> students = new ArrayList<>();
         List<Professor> professors = new ArrayList<>();
         List<String> errors = new ArrayList<>();
 
-        Map<String, Object> result = new HashMap<>();
-
         try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) { // 첫 번째 행은 헤더이므로 건너뜀
+
+            int dataRows = 0; // 데이터가 있는 행의 개수
+
+            // 데이터가 있는 행의 개수를 세는 반복문
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+                if (row != null && row.getCell(0) != null) {
+                    dataRows++; // 데이터가 있는 행이므로 개수를 증가시킴
+                }
+            }
+
+            for (int i = 1; i <= dataRows; i++) { // 첫 번째 행은 헤더이므로 건너뜀
                 MemberFormDto memberFormDto = new MemberFormDto();
 
                 Row row = sheet.getRow(i);
                 if (row != null) {
-                    if (row.getCell(1) == null) break;
-                    if (row.getCell(2) == null) break;
-                    if (row.getCell(3) == null) break;
-                    if (row.getCell(4) == null) break;
-                    if (row.getCell(5) == null) break;
                     memberFormDto.setRole(row.getCell(0).getStringCellValue());
                     memberFormDto.setName(row.getCell(1).getStringCellValue());
                     memberFormDto.setEmail(row.getCell(2).getStringCellValue());
@@ -64,9 +73,16 @@ public class ExcelService {
                     Errors validationErrors = new BeanPropertyBindingResult(memberFormDto, "memberFormDto");
                     memberFormDtoValidator.validate(memberFormDto, validationErrors);
 
+                    // 이메일 중복 검사
+                    if (!validationErrors.hasErrors()) {
+                        if (memberService.existsByEmail(memberFormDto.getEmail())) {
+                            validationErrors.rejectValue("email", "field.duplicate", "이미 등록된 이메일입니다.");
+                        }
+                    }
+
                     if (validationErrors.hasErrors()) {
                         StringBuilder errorMessage = new StringBuilder((i + 1) + "번째 행: ");
-                        validationErrors.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append("\n"));
+                        validationErrors.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()));
                         errors.add(errorMessage.toString());
                     } else {
                         switch (row.getCell(0).getStringCellValue()) {
@@ -85,13 +101,48 @@ public class ExcelService {
                         }
                     }
                 }
+
+                // 파일 읽는 것에 대한 진행 상황
+                int percentComplete = (int) ((i / (double) dataRows) * 100);
+                String message = "진행상황: " + (i + 1) + " / " + (dataRows + 1);
+                messagingTemplate.convertAndSend("/topic/progress", new ProgressUpdate(percentComplete, message));
             }
-            result.put("STAFF", staffs);
-            result.put("STUDENT", students);
-            result.put("PROFESSOR", professors);
-            result.put("errors", errors);
         }
 
-        return result;
+        if (!errors.isEmpty()) {
+            return errors.toString().replaceAll(",", "\n");
+        } else {
+            processDatabaseUpdates(staffs, students, professors, messagingTemplate);
+            return null;
+        }
+    }
+
+    private void processDatabaseUpdates(List<Staff> staffs, List<Student> students, List<Professor> professors, SimpMessagingTemplate messagingTemplate) {
+        // 학생 등록
+        for (int i = 0; i < students.size(); i++) {
+            memberService.createMember(students.get(i));
+
+            int percentComplete = (int) (((i + 1) / (double) students.size()) * 100);
+            String message = "학생 등록 중: " + (i + 1) + " / " + students.size();
+            messagingTemplate.convertAndSend("/topic/progress", new ProgressUpdate(percentComplete, message));
+        }
+
+        // 교수 등록
+        for (int i = 0; i < professors.size(); i++) {
+            memberService.createMember(professors.get(i));
+
+            int percentComplete = (int) (((i + 1) / (double) professors.size()) * 100);
+            String message = "교수 등록 중: " + (i + 1) + " / " + professors.size();
+            messagingTemplate.convertAndSend("/topic/progress", new ProgressUpdate(percentComplete, message));
+        }
+
+        // 교직원 등록
+        for (int i = 0; i < staffs.size(); i++) {
+            memberService.createMember(staffs.get(i));
+
+            int percentComplete = (int) (((i + 1) / (double) staffs.size()) * 100);
+            String message = "교직원 등록 중: " + (i + 1) + " / " + staffs.size();
+            messagingTemplate.convertAndSend("/topic/progress", new ProgressUpdate(percentComplete, message));
+        }
     }
 }
