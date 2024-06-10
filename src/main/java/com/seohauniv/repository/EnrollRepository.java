82c7d412ddb633f99580d30483514c9a3a29cf4f
volkinuna <plleiades@gmail.com>
2024-06-10 16:13:48 +0900

    내 강의 목록 구현

--- src/main/java/com/seohauniv/repository/EnrollRepository.java
+++ src/main/java/com/seohauniv/repository/EnrollRepository.java
@@ -3,11 +3,22 @@ package com.seohauniv.repository;
 import com.seohauniv.entity.Course;
 import com.seohauniv.entity.Enroll;
 import com.seohauniv.entity.Student;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EnrollRepository extends JpaRepository<Enroll, Long> {
     List<Enroll> findByStudent(Student student);
+
+    //현재 로그인한 학생의 수강 신청내역을 페이징 조건에 맞춰서 조회
+    @Query("select e from Enroll e where e.student.id = :memberId")
+    List<Enroll> findEnrolls(@Param("memberId") String memberId, Pageable pageable);
+
+    //현재 로그인한 학생의 수강 신청 갯수가 몇개인지 조회(total)
+    @Query("select count(e) from Enroll e where e.student.id = :memberId")
+    Long countEnroll(@Param("memberId") String memberId);
 }
